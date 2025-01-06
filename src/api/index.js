13b777f71736cb2f5e8f5a7353d392024da01443
FERANMI <168641833+Feranmidevelops@users.noreply.github.com>
2025-01-06 21:32:50 +0100

    fixed the horizontal scrolling for mobile

--- src/api/index.js
+++ src/api/index.js
@@ -3,53 +3,49 @@ import axios from "axios";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export async function getWeatherData(endpoint, place_id, measurementSystem) {
-  console.log('measurementSystem', measurementSystem);
-  
+  console.log("measurementSystem", measurementSystem);
+
   const options = {
-    method: 'GET',
+    method: "GET",
     url: `https://ai-weather-by-meteosource.p.rapidapi.com/${endpoint}`,
     params: {
       place_id,
-      timezone: 'auto',
-      language: 'en',
+      timezone: "auto",
+      language: "en",
       units: measurementSystem,
     },
     headers: {
-      'x-rapidapi-key': API_KEY,
-      'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com'
-    }
+      "x-rapidapi-key": API_KEY,
+      "x-rapidapi-host": "ai-weather-by-meteosource.p.rapidapi.com",
+    },
   };
-  
+
   try {
     const response = await axios.request(options);
     return response.data;
-    
   } catch (error) {
     console.error(error);
   }
 }
 
 export async function searchPlaces(text) {
+  const options = {
+    method: "GET",
+    url: "https://ai-weather-by-meteosource.p.rapidapi.com/find_places",
+    params: {
+      text,
+      language: "en",
+    },
+    headers: {
+      "x-rapidapi-key": API_KEY,
+      "x-rapidapi-host": "ai-weather-by-meteosource.p.rapidapi.com",
+    },
+  };
 
-  
-const options = {
-  method: 'GET',
-  url: 'https://ai-weather-by-meteosource.p.rapidapi.com/find_places',
-  params: {
-    text,
-    language: 'en'
-  },
-  headers: {
-    'x-rapidapi-key': API_KEY,
-    'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com'
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
-};
-
-try {
-	const response = await axios.request(options);
-	return response.data;
-} catch (error) {
-	console.error(error);
-}
-  
 }
